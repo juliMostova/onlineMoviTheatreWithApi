db8de405c716e570d6df2073a53dfbf897dfdab4
Juli Mostova <juli.mostova89@gmail.com>
2024-07-13 12:48:18 +0300

    change a picture

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -3,7 +3,7 @@ import ModalRegistration from '../Modal/ModalRegistration';
 
 function Header (){
 return(
-<span className='header'onClick={()=>window.scroll(0,0)}>🎬 Entertaiment Movi 🎥
+<span className='header'onClick={()=>window.scroll(0,0)}>🎥 Entertaiment Movi 🍿
  <ModalRegistration />
  </span>
 );
